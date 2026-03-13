cd927ed6c1a3082d16c2cae4298b4c2564467eff
Zack McKenna <zack.mckenna@runpod.io>
2026-03-13 15:57:31 -0400

    fix: use unique template names in examples to prevent CI test collisions
    Append the Pulumi stack name to template and volume names so parallel
    test runs and re-runs don't fail with "Template name must be unique".

--- examples/dotnet/Program.cs
+++ examples/dotnet/Program.cs
@@ -6,7 +6,7 @@ return await Deployment.RunAsync(() =>
 {
     var myTemplate = new Runpod.Template("myTemplate", new()
     {
-        Name = "my-pulumi-template",
+        Name = $"my-pulumi-template-{Deployment.Instance.StackName}",
         ImageName = "runpod/pytorch:2.1.0-py3.10-cuda11.8.0-devel-ubuntu22.04",
         ContainerDiskInGb = 20,
         VolumeInGb = 20,
